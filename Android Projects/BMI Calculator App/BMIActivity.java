e9c371fb9824da9a0bb44259b5385c4a413750cf
aiyan981 <61669410+aiyan981@users.noreply.github.com>
2020-03-01 20:03:27 -0500

    Create BMIActivity.java

---
+++ Android Projects/BMI Calculator App/BMIActivity.java
@@ -0,0 +1,32 @@
+package com.example.user.bmi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class BMIActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bmi_layout);
+    }
+
+    public void buttonClicked(View v) {
+        EditText weightView = (EditText) findViewById(R.id.weightBox);
+        String weight = weightView.getText().toString();
+        EditText heightView = (EditText) findViewById(R.id.heightBox);
+        String height = heightView.getText().toString();
+
+        BMIModel myModel = new BMIModel(weight, height);
+        String myBMI = myModel.getBMI();
+        String heighto = myModel.toFeetInch();
+        String weighto = myModel.toPound();
+        String out = "Your weight is " + weighto + " lb, your height is " + heighto +
+                ", and your BMI is " + myBMI;
+        ((TextView) findViewById(R.id.answer)).setText(out);
+    }
+
+}
